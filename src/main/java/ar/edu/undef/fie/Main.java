0fb9788513efe08e6a02f6c34877496c8d683974
“julioleonel” <jsendra@fie.undef.edu.ar>
2023-09-11 09:06:32 -0300

    Primer commit con enunciado

---
+++ src/main/java/ar/edu/undef/fie/Main.java
@@ -0,0 +1,20 @@
+
+
+package ar.edu.undef.fie;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    }
+}
+
+
+
+
+
+/*
+2. Hacer un programa que defina una lista de números enteros, y tenga una función booleana que recibe una lista y un número entero.
+La función deberá devolver verdadero si el elemento pasado por parámetro existe.
+ */
