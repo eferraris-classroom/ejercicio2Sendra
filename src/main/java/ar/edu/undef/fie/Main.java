0b6f2c8a24afc115a054f303a536f54e6e416565
“julioleonel” <jsendra@fie.undef.edu.ar>
2023-09-11 10:00:17 -0300

    codigo con error

--- src/main/java/ar/edu/undef/fie/Main.java
+++ src/main/java/ar/edu/undef/fie/Main.java
@@ -1,20 +1,31 @@
+/*
+2. Hacer un programa que defina una lista de números enteros, y tenga una función booleana que recibe una lista y un número entero.
+La función deberá devolver verdadero si el elemento pasado por parámetro existe.
 
+*/
 
 package ar.edu.undef.fie;
 
 public class Main {
     public static void main(String[] args) {
 
+        int [] numerosEnteros = {4, 5, 6, 3, 2, 4};
+        int valor = 6;
 
+        for (int numero : numerosEnteros) {
+                if (numero == valor) {
+                    return True;
 
+                }
+        }
+
+        }
     }
-}
 
 
 
 
 
-/*
-2. Hacer un programa que defina una lista de números enteros, y tenga una función booleana que recibe una lista y un número entero.
-La función deberá devolver verdadero si el elemento pasado por parámetro existe.
- */
+
+
+
